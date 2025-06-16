1b4ace6ddaf2245500b54aa8dd3ee52f17432dc0
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-06-17 02:45:33 +0530

    Create sum.py

---
+++ Practice/sum.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+# the output of this code will be: 0.8 
+print(a/b)    
+# the output of this code will be: 0.8         
+print(int(a)/int(b))
+# the output of this code will be: 0
+print(a//b)
+
+    # To get both integer division and float division results in Python, use:
+
+    # a // b` for integer division (quotient, discards remainder)
+    # a / b` for float division (returns decimal result)
