aefe5da4ca38486a831a0a9998f58ea579a18668
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-06-17 03:02:53 +0530

    Create forloop2.py

---
+++ Practice/forloop2.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, "tamatar"]
+for num in numbers:
+    print(num)
+
+# This loop iterates over each element in the numbers list.
+# On each iteration, the variable num takes the value of the next item in the list.
