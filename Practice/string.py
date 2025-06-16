6271a8d1cf0a39c1b1d603e9cd09c3c6fa7dca8e
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-06-17 01:00:05 +0530

    Create string.py

---
+++ Practice/string.py
@@ -0,0 +1,5 @@
+#  Python can be passed arbitrary code as a string in the shell:
+#   python -c 'print("Hello, World")'
+#  Hello, World
+#  This can be useful when concatenating the results of scripts together in the shell. 
+# this will work in cmd mode
