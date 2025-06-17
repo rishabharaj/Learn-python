6e37dfc186fed8795fd16c66383f62e05af2622a
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-06-17 20:16:43 +0530

    Add files via upload

---
+++ Practice/numpy_test.py
@@ -0,0 +1,5 @@
+import numpy as np
+# Example of creating a NumPy array
+
+my_array = np.array([1, 2, 3, 4])  # NumPy array initialization
+print(type(my_array), my_array)  # Output: <class 'numpy.ndarray'> [1 2 3 4]
