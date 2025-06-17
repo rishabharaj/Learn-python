1722f4b2943de81e7746bb569a4f500610fc75d7
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-06-17 20:18:47 +0530

    Add files via upload

---
+++ Practice/array.py
@@ -0,0 +1,4 @@
+import array
+
+my_array = array.array('i', [1, 2, 3, 4])  # 'i' integer type represent karta hai
+print(type(my_array), my_array)  # Output: <class 'array.array'> array('i', [1, 2, 3, 4])
