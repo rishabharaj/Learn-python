2f41282006b327dcca4860c80d145e5eec00dded
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-06-26 02:47:11 +0530

    Create f_string_print.py

---
+++ Practice/f_string_print.py
@@ -0,0 +1,8 @@
+w = 100
+b = 100
+print(f"w: {w}")
+print(f"b: {b}")
+
+# F-String → Smart Digital Template
+# Jaise ek auto-filled email template
+# jo dynamic values khud daal deta hai—smart, fast, aur readable.
